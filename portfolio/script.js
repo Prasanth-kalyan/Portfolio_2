8d0b124ebb1fe0113199deaa024d90a41d11d989
Prasanth <prasanthkalyan2@gmail.com>
2024-10-20 22:55:37 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! Thank you for contacting me.');
+    document.getElementById('contactForm').reset();
+});
